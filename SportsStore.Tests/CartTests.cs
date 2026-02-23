43d05d1174ac5eea74932a1e9ea0a81551c50842
Temka <Temka9043@gmail.com>
2026-02-23 14:50:03 +0000

    Part A complete with this push 0 warnings and 0 errors.

--- SportsStore.Tests/CartTests.cs
+++ SportsStore.Tests/CartTests.cs
@@ -68,7 +68,7 @@ namespace SportsStore.Tests {
             target.RemoveLine(p2);
 
             // Assert
-            Assert.Empty(target.Lines.Where(c => c.Product == p2));
+            Assert.DoesNotContain(target.Lines, l => l.Product.ProductID == 1);
             Assert.Equal(2, target.Lines.Count());
         }
 
